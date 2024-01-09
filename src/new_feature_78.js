c608b6e5ba9288d810390dd086ada6d00b9f2770
C-Green-Code <shallowgreenjob@gmail.com>
2024-01-09 21:32:25 +0800

    fix: fix inconsistent reward distribution

--- src/new_feature_78.js
+++ src/new_feature_78.js
@@ -1,3 +1,4 @@
 // fix: correct player experience calculation - 2025年 3月12日 星期三 18时20分12秒 CST
 console.log('fix: correct player experience calculation');
 if (Math.random() > 0.5) console.warn('Potential issue detected');
+return null;
