a0d6ab731e20c4c3277fbe3ab9c65c1284e4e1af
joji-mb <545633740@qq.com>
2024-03-08 12:18:31 +0800

    fix: fix missing item descriptions

--- src/new_feature_40.js
+++ src/new_feature_40.js
@@ -1,2 +1,3 @@
 // feat: implement reward streak bonuses - 2025年 3月12日 星期三 18时20分11秒 CST
 console.log('Debug: fix: fix missing item descriptions');
+console.log('Debug: fix: fix missing item descriptions');
