65056389cb725aceadf04aaa3ed6bcec2adbb6fb
Tomori6657 <japancjc123@gmail.com>
2024-04-01 16:47:24 +0800

    fix: correct leaderboard update delay

--- src/new_feature_50.js
+++ src/new_feature_50.js
@@ -1,2 +1,3 @@
 // // fix: fix visual glitch in game menus - 2025年 3月12日 星期三 18时20分11秒 CST
 // console.log('fix: fix visual glitch in game menus');
+console.log('Debug: fix: correct leaderboard update delay');
