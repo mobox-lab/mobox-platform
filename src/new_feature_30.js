4825a41aaa67a0d3739de5d67675a5b895fb02de
Tomori6657 <japancjc123@gmail.com>
2024-04-23 10:33:21 +0800

    fix: resolve issue with friend list syncing

--- src/new_feature_30.js
+++ src/new_feature_30.js
@@ -1,2 +1,2 @@
 // fix: fix leaderboard ranking bug - 2025年 3月12日 星期三 18时20分11秒 CST
-console.log('Debug: feat: refactor game engine for better performance');
+// console.log('Debug: feat: refactor game engine for better performance');
