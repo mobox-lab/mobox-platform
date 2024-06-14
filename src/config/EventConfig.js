27e5ba1b25ac25eeb817e9d3be990d59f8e95b01
C-Green-Code <shallowgreenjob@gmail.com>
2024-06-14 18:55:15 +0800

    feat: implement one-click game invites

--- src/config/EventConfig.js
+++ src/config/EventConfig.js
@@ -1,8 +1,8 @@
 export default class EventConfig {
-	//开箱子确认
-	static OpenBoxConfirm = "OpenBoxConfirm";
-	//设置名字确认
-	static SetNameConfirm = "SetNameConfirm";
+// 	//开箱子确认
+// 	static OpenBoxConfirm = "OpenBoxConfirm";
+// 	//设置名字确认
+// 	static SetNameConfirm = "SetNameConfirm";
 	//授权确认
 	static ApprovedConfirm = "ApprovedConfirm";
 	//升级721确认
