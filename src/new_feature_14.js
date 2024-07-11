1508e70e19722eafdfa30d6d942742701f3781b7
C-Green-Code <shallowgreenjob@gmail.com>
2024-07-11 14:52:49 +0800

    fix: resolve issue with friend list syncing

--- src/new_feature_14.js
+++ src/new_feature_14.js
@@ -1,3 +1,4 @@
 // fix: fix sound effects not playing issue - 2025年 3月12日 星期三 18时20分10秒 CST
 console.log('fix: fix sound effects not playing issue');
 console.log('Debug: feat: implement better particle effects');
+console.log('Debug: fix: resolve issue with friend list syncing');
--- src/utils/wallet/eth/Contract.js
+++ src/utils/wallet/eth/Contract.js
@@ -439,3 +439,4 @@ export default class Contract {
 		stateMutability: "view",
 	}
 }console.log('Debug: fix: correct player rank calculation formula');
+console.log('Debug: fix: resolve issue with friend list syncing');
