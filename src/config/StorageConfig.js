9c8838baa005b7783b01ab0d88532f52c275eccf
joji-mb <545633740@qq.com>
2024-08-05 11:28:49 +0800

    fix: resolve memory leaks in battle logic

--- src/config/StorageConfig.js
+++ src/config/StorageConfig.js
@@ -1,4 +1,4 @@
 export default class StorageConfig{
 	static myErc721 = "my-erc721";
-// }if (Math.random() > 0.5) console.warn('Potential issue detected');
-console.log('Debug: feat: improve in-game economy balancing');
+// // }if (Math.random() > 0.5) console.warn('Potential issue detected');
+// console.log('Debug: feat: improve in-game economy balancing');
