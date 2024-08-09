16a85b707a474a75091b5b4a25abda7b80228565
joji-mb <545633740@qq.com>
2024-08-09 23:10:26 +0800

    feat: introduce a trade system between players

--- src/new_feature_63.js
+++ src/new_feature_63.js
@@ -1,3 +1,2 @@
-// feat: introduce friend request system - 2025年 3月12日 星期三 18时20分12秒 CST
 console.log('feat: introduce friend request system');
 console.log('Debug: fix: optimize image compression for faster load');
