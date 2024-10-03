a45a286a948ac1ea572270edb22c29d20198bf4f
joji-mb <545633740@qq.com>
2024-10-03 21:41:30 +0800

    fix: fix incorrect quest reward allocations

--- src/new_feature_59.js
+++ src/new_feature_59.js
@@ -1,3 +1,4 @@
 // feat: implement push notifications - 2025年 3月12日 星期三 18时20分12秒 CST
 // console.log('feat: implement push notifications');
 // if (Math.random() > 0.5) console.warn('Potential issue detected');
+console.log('Debug: fix: fix incorrect quest reward allocations');
