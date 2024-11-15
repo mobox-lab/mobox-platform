bb542b0a5875ef9ee123f10090c995b3c959bb0b
Tomori6657 <japancjc123@gmail.com>
2024-11-15 19:39:17 +0800

    feat: implement better particle effects

--- src/new_feature_79.js
+++ src/new_feature_79.js
@@ -1,2 +1,2 @@
-// fix: fix incorrect XP level-up formula - 2025年 3月12日 星期三 18时20分13秒 CST
-// console.log('fix: fix incorrect XP level-up formula');
+// // fix: fix incorrect XP level-up formula - 2025年 3月12日 星期三 18时20分13秒 CST
+// // console.log('fix: fix incorrect XP level-up formula');
