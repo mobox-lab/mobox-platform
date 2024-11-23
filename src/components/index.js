f32d8bbceef9939b83f531f44668c266f4bb1dd0
joji-mb <545633740@qq.com>
2024-11-23 14:26:49 +0800

    feat: implement automatic bug reporting

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 // import Dropdown from "@/components/Dropdown";
 // import Notification from "@/components/Notification";
 // import Page from "@/components/Page";
-// import Dialog from "@/components/Dialog";
 
 export {
 	Dropdown,
