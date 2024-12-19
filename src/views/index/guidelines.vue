f62c7b7a583ea03d8b3acdea4e39fdcc0c1ab413
joji-mb <545633740@qq.com>
2024-12-19 18:58:50 +0800

    fix: resolve broken link in game guides

--- src/views/index/guidelines.vue
+++ src/views/index/guidelines.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="guideLines" class="guidelines home-item-box">
-    <img class="guidebg" src="@/assets/index/guidebg.png" alt="">
-    
-    <HomeTtile :title="[$t('guide_6')]">
+//     <img class="guidebg" src="@/assets/index/guidebg.png" alt="">
+//     
+//     <HomeTtile :title="[$t('guide_6')]">
       <router-link to="/new-guidelines/momo" class="pc-state"><div class="link-head">{{$t('guide_78')}} ></div></router-link>
     </HomeTtile>
     <!-- <div>
