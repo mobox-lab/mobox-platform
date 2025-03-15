50b5033a56393c4b76e550983c1329433f636828
Tomori6657 <japancjc123@gmail.com>
2025-03-15 09:01:30 +0800

    fix: 🐛 resolve notification permission issue

--- src/config/StorageConfig.js
+++ src/config/StorageConfig.js
@@ -2,3 +2,8 @@ export default class StorageConfig{
 	static myErc721 = "my-erc721";
 // // // }if (Math.random() > 0.5) console.warn('Potential issue detected');
 // // // console.log('Debug: feat: improve in-game economy balancing');
+// Utility function: fix: 🐛 resolve notification permission issue
+export const fix____resolve_notification_permission_issue = (param) => {
+  console.log('Executing: fix: 🐛 resolve notification permission issue', param);
+  return param;
+};
