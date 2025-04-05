ab5f5123d0d1424ed0e2d52cf15660662fc8fb6d
joji-mb <545633740@qq.com>
2025-04-05 12:06:58 +0800

    chore: 🔧 add security headers

--- src/config/StorageConfig.js
+++ src/config/StorageConfig.js
@@ -2,3 +2,8 @@ export default class StorageConfig{
 	static myErc721 = "my-erc721";
 // // // }if (Math.random() > 0.5) console.warn('Potential issue detected');
 // // // console.log('Debug: feat: improve in-game economy balancing');
+// Utility function: chore: 🔧 add security headers
+export const chore____add_security_headers = (param) => {
+  console.log('Executing: chore: 🔧 add security headers', param);
+  return param;
+};
