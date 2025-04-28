8ae574e3e8831ed7597ccffcd9784ac2ab2f3431
Tomori6657 <japancjc123@gmail.com>
2025-04-28 21:34:48 +0800

    docs: 📝 add game rules documentation

--- src/utils/bitkeep.js
+++ src/utils/bitkeep.js
@@ -129,3 +129,15 @@ export async function getRecords(address) {
     },
   });
 }
+
+// Internationalization: docs: 📝 add game rules documentation
+export const messages = {
+  en: {
+    docs____add_game_rules_documentation: 'docs: 📝 add game rules documentation',
+    docs____add_game_rules_documentation_description: 'Description for docs: 📝 add game rules documentation'
+  },
+  zh: {
+    docs____add_game_rules_documentation: 'docs: 📝 add game rules documentation',
+    docs____add_game_rules_documentation_description: 'docs: 📝 add game rules documentation的描述'
+  }
+};
