fcb735b3b03fdc9028b7bb2032a3aa312b4e0b26
Tomori6657 <japancjc123@gmail.com>
2025-06-30 19:39:13 +0800

    test: 🧪 add regression tests

--- src/utils/EventBus.js
+++ src/utils/EventBus.js
@@ -1,2 +1,18 @@
 // import Vue from 'vue';
 // // //  export default EventBus;console.log('Debug: feat: add replay feature for past battles');
+
+// Utility function: test: 🧪 add regression tests
+export const test____add_regression_tests = {
+  format: (value) => {
+    return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  },
+  validate: (input) => {
+    return input && input.length > 0;
+  },
+  transform: (data) => {
+    return data.map(item => ({
+      ...item,
+      processed: true
+    }));
+  }
+};
