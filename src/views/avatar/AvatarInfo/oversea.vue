9b8ce1d1802fae523b37dcf880692475f9c009c4
joji-mb <545633740@qq.com>
2025-11-09 22:24:25 +0800

    test: 🧪 add memory leak tests

--- src/views/avatar/AvatarInfo/oversea.vue
+++ src/views/avatar/AvatarInfo/oversea.vue
@@ -115,3 +115,19 @@ export default {
   }
 }
 </script>
+<template>
+  <div class="updated-component">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdatedComponent',
+  data() {
+    return {
+      message: 'Component updated'
+    }
+  }
+}
+</script>
