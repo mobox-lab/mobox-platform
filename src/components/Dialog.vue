6dd2938df3f0188ce66cc251afb4a35bc1fdbbc2
joji-mb <545633740@qq.com>
2025-11-17 22:36:35 +0800

    refactor: 🔧 restructure data models

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -117,3 +117,19 @@ export default {
     transform: translateY(-30px);
   }
 </style>
+<template>
+  <div class="updated-component">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdatedComponent',
+  data() {
+    return {
+      message: 'Component updated'
+    }
+  }
+}
+</script>
