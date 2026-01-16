eb9ab24abc08abccd99f768628cbe324608cb299
joji-mb <545633740@qq.com>
2026-01-16 09:22:06 +0800

    test: 🧪 add cross-browser tests

--- src/components/Header/Register/UserAgreement/AgreementButton.vue
+++ src/components/Header/Register/UserAgreement/AgreementButton.vue
@@ -57,3 +57,19 @@ export const newFeature = () => {
   console.log('Feature implemented successfully');
   return true;
 };
+<template>
+  <div class="updated-component">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdatedComponent',
+  data() {
+    return {
+      message: 'Component updated'
+    }
+  }
+}
+</script>
